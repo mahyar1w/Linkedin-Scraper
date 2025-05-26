af42e952a0a6e55df56ae88fc03b566c4efabc02
mahyar <mma1285ma@gmail.com>
2025-05-26 12:18:26 +0330

    Fixing an oopsie

--- datacollector.py
+++ datacollector.py
@@ -12,10 +12,10 @@ driver.get("https://www.linkedin.com/login")
 time.sleep(2)
 
 username = driver.find_element(By.ID, "username")
-username.send_keys("ma1285moh@gmail.com")
+username.send_keys("something")
 
 password = driver.find_element(By.ID, "password")
-password.send_keys("mahyar12")
+password.send_keys("something")
 
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
@@ -24,13 +24,16 @@ time.sleep(5)
 try:
     search_box = driver.find_element(By.XPATH, '//*[@id="global-nav-typeahead"]/input')
     search_box.click()
+
 except:
     driver.find_element(By.XPATH, '//*[@id="global-nav-search"]/div/button').click()
     search_box = driver.find_element(By.XPATH, '//*[@id="global-nav-typeahead"]/input')
     search_box.click()
-    
+
 search_box.send_keys("Python Developer")
 search_box.send_keys(Keys.RETURN)
+    
+
 
 time.sleep(5)
 
