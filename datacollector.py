7750da0669a207642397257c3b3aa9371398dd92
stivenpars <stivenpars@outlook.com>
2025-06-05 19:02:16 +0330

    code now extracts information but is not organized

--- datacollector.py
+++ datacollector.py
@@ -1,44 +1,63 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
 import time
 
-service = Service(executable_path='/usr/bin/chromedriver')
-driver = webdriver.Chrome(service=service)
-
-driver.get("https://www.linkedin.com/login")
-
+driver = webdriver.Chrome()
+driver.get('https://www.linkedin.com/login')
 time.sleep(2)
 
 username = driver.find_element(By.ID, "username")
-username.send_keys("something")
+username.send_keys("email")
 
 password = driver.find_element(By.ID, "password")
-password.send_keys("something")
+password.send_keys("password")
 
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
+time.sleep(15)
 
-time.sleep(5)
+driver.get('https://www.linkedin.com/jobs/search?trk=guest_homepage-basic_guest_nav_menu_jobs&position=1&pageNum=0')
+time.sleep(3)
 
-try:
-    search_box = driver.find_element(By.XPATH, '//*[@id="global-nav-typeahead"]/input')
-    search_box.click()
+job_title="data scientist"
 
-except:
-    driver.find_element(By.XPATH, '//*[@id="global-nav-search"]/div/button').click()
-    search_box = driver.find_element(By.XPATH, '//*[@id="global-nav-typeahead"]/input')
-    search_box.click()
+country = "united states"
 
-search_box.send_keys("Python Developer")
-search_box.send_keys(Keys.RETURN)
-    
+#number_of_results = 
+
+job_search_bar = driver.find_element(By.CSS_SELECTOR, """input[aria-label="Search by title, skill, or company"]""")
+job_search_bar.send_keys(job_title)
+
+country_search_bar = driver.find_element(By.CSS_SELECTOR, """input[aria-label="City, state, or zip code"]""")
+country_search_bar.send_keys(Keys.CONTROL + "a")
+country_search_bar.send_keys(Keys.DELETE)
+country_search_bar.send_keys(country+Keys.ENTER)
+time.sleep(4)
+
+jobs_window = driver.find_element(By.CSS_SELECTOR, "#main > div > div.scaffold-layout__list-detail-inner.scaffold-layout__list-detail-inner--grow > div.scaffold-layout__list > div")
+
+for i in range(20):
+    driver.execute_script("arguments[0].scrollBy(0, 500);", jobs_window)
+    #jobs_window.send_keys(Keys.PAGE_DOWN)
+    time.sleep(0.6)
 
 
-time.sleep(5)
+jobs = driver.find_elements(By.CLASS_NAME,"artdeco-entity-lockup__title")
+for job in jobs:
+    print(job.text)
+companies = driver.find_elements(By.CLASS_NAME,"artdeco-entity-lockup__subtitle")
+for company in companies:
+    print(company.text)
+locations = driver.find_elements(By.CLASS_NAME,"artdeco-entity-lockup__caption")
+for location in locations:
+    print(location.text)
+links = driver.find_elements(By.CLASS_NAME,"job-card-container__link")
+for link in links:
+    print(link.get_attribute("href"))
 
-profiles = driver.find_elements(By.XPATH, "//span[@dir='ltr']/span[@aria-hidden='true']")
-for profile in profiles:
-    print(profile.text)
+#driver.find_element(By.CSS_SELECTOR,"""button[aria-label="View next page"]""").click()
+
+time.sleep(3)
 
 driver.quit()
